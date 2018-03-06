72bc95eb2eedef87d9227d015ff8e12d6b84e639
Arthur Guo <arthur.guo@uwaterloo.ca>
2018-03-06 16:10:56 +0000

    Add logging and downloads ads

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 let { query, scrape } = require( "kijiji-scraper" );
 var crypto = require("crypto");
-var request = require("request")
-var fs = require("fs")
+var request = require("request");
+var fs = require("fs");
+var winston = require("winston");
+var http = require('https'),                                                
+    Stream = require('stream').Transform;
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
 
 
 var titles = [];
@@ -50,35 +66,51 @@ function scrapeCar(url){
 }
 
 function scrapeImage(car){
-   let images = car.images;
-   new_images = [];
-   for (var i in images){
-      var id = crypto.randomBytes(20).toString('hex') + ".jpg";
-      //console.log(id);
-      download(images[i], "photos/"+id);
-      new_images.push(id);
-   }  
-   car.images = new_images;
+   return new Promise(function(resolve, reject){
+     let images = car.images;
+     new_images = [];
+     let downloadPromise = Promise.all(images.map(download));
+     downloadPromise.then(function(new_images){
+       car.images = new_images;
+       resolve(car);
+     });
+     downloadPromise.catch(reject);
+   });
+
 }
 
-function download(uri, filename){
+function download(uri){
+  var id = crypto.randomBytes(20).toString('hex') + ".jpg";
+  var filename = "photos/"+id;
   return new Promise(function (resolve, reject){
-     request.head(uri, function(err, res, body){
-       if(err){
-          reject(err);
-       }
-       request(uri).pipe(fs.createWriteStream(filename)).on('close', resolve);
-     });
+    http.request(uri, function(response) {                                        var data = new Stream();                                                    
+
+      response.on('data', function(chunk) {                                       
+        data.push(chunk);                                                         
+      });                                                                         
+
+      response.on('end', function() {                                             
+        fs.writeFileSync(filename, data.read());                               
+        resolve();
+      });                                                                         
+    }).on("error", reject).end();
   });
 };
 
-function saveCar(){
-
+function saveCar(carData){
+  logger.log({
+    level: 'info',
+    data: carData
+  });
 }
 
-getOntarioCars()
-  .then(collectUnseenItems)
-  .then(scrapeEachCar);
+function doScrape(){
+  getOntarioCars()
+    .then(collectUnseenItems)
+    .then(scrapeEachCar);
+}
+
+setInterval(doScrape, 3000);
 /*
 scrapeCar("https://www.kijiji.ca/v-cars-trucks/city-of-toronto/2014-ram-1500-sxt-quad-cab-4x4-5-7l-hemi/1336810628").then(console.log)
 */
